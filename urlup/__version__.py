3d78474326dc77ad025998a63ea342d27c3b5de3
Michael Hucka <mhucka@caltech.edu>
2018-04-06 09:50:09 -0700

    First complete __version__.py file

---
+++ urlup/__version__.py
@@ -0,0 +1,16 @@
+# =============================================================================
+# @file    __version__.py
+# @brief   urlup version info
+# @author  Michael Hucka <mhucka@caltech.edu>
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/caltechlibrary/urlup
+# =============================================================================
+
+__version__     = '1.0.0'
+__title__       = 'urlup'
+__description__ = 'URL Update'
+__url__         = 'https://github.com/caltechlibrary/urlup'
+__author__      = 'Michael Hucka <mhucka@caltech.edu>'
+__email__       = 'mhucka@caltech.edu'
+__license__     = 'BSD/MIT derivative -- see LICENSE file'
+__copyright__   = 'Copyright (C) 2018 by the California Institute of Technology'
