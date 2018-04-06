773416f30f4892fd50c5d154d2f917ee1182be87
Michael Hucka <mhucka@caltech.edu>
2018-04-06 09:49:42 -0700

    First complete version of an __init__.py file

---
+++ urlup/__init__.py
@@ -0,0 +1,25 @@
+'''
+urlup: Find the ultimate destination for URLs after following redirections.
+
+Authors
+-------
+
+Michael Hucka <mhucka@caltech.edu>
+
+Copyright
+---------
+
+Copyright (c) 2018 by the California Institute of Technology.  This open-source
+software is made freely available under the terms specified in the LICENSE file
+provided with this software.
+'''
+
+from .__version__ import __version__, __title__, __description__, __url__
+from .__version__ import __author__, __email__
+from .__version__ import __license__, __copyright__
+
+# Main modules.
+from .urlup import updated_urls
+
+# Supporting modules.
+from .messages import msg, color
