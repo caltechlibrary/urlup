3d62fa838b82462518b70a3c5ed596da7c6b4ba6
Michael Hucka <mhucka@caltech.edu>
2018-04-18 15:34:09 -0700

    Add simple utility to run urlup in debug mode

---
+++ tests/urlup_debug.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# =============================================================================
+# @file    urlup_debug.py
+# @brief   Run urlup with debug enabled
+# @author  Michael Hucka <mhucka@caltech.edu>
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/caltechlibrary/urlup
+# =============================================================================
+
+import os
+import sys
+import plac
+
+# Allow this program to be executed directly from the 'tests' directory.
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+import logging
+logging.basicConfig(level=logging.DEBUG, format='')
+
+import urlup
+plac.call(urlup.cli_main)
