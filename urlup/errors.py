7fec5ff8049cb5141559d6ecfff8e1e091641bd9
Michael Hucka <mhucka@caltech.edu>
2018-06-21 12:36:43 -0700

    Add NetworkError and ProxyLoginError

--- urlup/errors.py
+++ urlup/errors.py
@@ -2,6 +2,14 @@
 errors.py: exceptions defined by Urlup
 '''
 
+class NetworkError(Exception):
+    '''Class of Urlup exceptions involving network operations.'''
+    pass
+
 class ProxyException(Exception):
     '''Class of exceptions related to handling proxies.'''
+    pass
+
+class ProxyLoginError(ProxyException):
+    '''Could not log in to designated proxy host.'''
     pass
