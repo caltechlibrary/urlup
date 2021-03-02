f7d478349255e31d4efcd5b259d0cbc8c41baf9d
Michael Hucka <mhucka@caltech.edu>
2021-03-01 18:24:49 -0800

    Version bump

--- urlup/__version__.py
+++ urlup/__version__.py
@@ -6,11 +6,11 @@
 # @website https://github.com/caltechlibrary/urlup
 # =============================================================================
 
-__version__     = '1.4.2'
+__version__     = '1.5.0'
 __title__       = 'urlup'
 __description__ = 'A small utility program and Python 3 module to dereference URLs and determine their final destinations after following redirections.'
 __url__         = 'https://github.com/caltechlibrary/urlup'
 __author__      = 'Michael Hucka <mhucka@caltech.edu>'
 __email__       = 'mhucka@caltech.edu'
 __license__     = 'BSD 3-clause license -- see LICENSE file'
-__copyright__   = 'Copyright (C) 2018 by the California Institute of Technology'
+__copyright__   = 'Copyright (C) 2018-2021 by the California Institute of Technology'
